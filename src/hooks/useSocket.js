a23a5114227aced91014eb69402fcab1e17a0f1b
PRIYANSHU RAJ <priyanshue722@gmail.com>
2026-03-14 13:11:58 +0530

    Update SOCKET_URL to production endpoint

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://real-time-chat-application-backend-fmim.onrender.com";
 
 export const useSocket = (userId) => {
   const [socket, setSocket] = useState(null);
