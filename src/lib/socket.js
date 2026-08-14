32e59ccb2be10a4794d7c1553aa89469f5b92d8f
ITZ-PRIYANSHU-RAJ <priyanshue722@gmail.com>
2026-08-14 12:22:49 +0530

    client-socket

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,8 +1,6 @@
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
-const SOCKET_URL= "http://localhost/5000";
-
-export const socket = io(SOCKET_URL,{
-    autoConnect:false,
-    withCredentials:true,
+export const socket = io("http://localhost:5000", {
+  autoConnect: false,
+  withCredentials: true,
 });
